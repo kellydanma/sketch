31f25c09e88178da9eb3ff85b04b393b51e695df
Kelly Ma <kelly.ma@mail.mcgill.ca>
2021-01-06 11:13:15 -0500

    Implement mouseover effect

--- scripts.js
+++ scripts.js
@@ -1,14 +1,38 @@
-function createGrid() {
+function createGrid(id, container, width) {
+  const grid = document.createElement("div");
+  grid.className = "grid";
+  grid.id = id.toString();
+  grid.style.backgroundColor = "black";
+  grid.style.height = width;
+  container.appendChild(grid);
+}
+
+function createContainer() {
   const container = document.querySelector(".container");
   const width = ((container.offsetWidth - 20) / 10).toString() + "px";
   for (let i = 0; i < 100; i++) {
-    const grid = document.createElement("div");
-    grid.className = "grid";
-    grid.id = i.toString();
-    grid.style.backgroundColor = "black";
-    grid.style.height = width;
-    container.appendChild(grid);
+    createGrid(i, container, width);
   }
 }
 
-createGrid();
+function handleHover() {
+  const grids = document.getElementsByClassName("grid");
+  Array.from(grids).forEach((grid) => {
+    grid.addEventListener(
+      "mouseover",
+      function (event) {
+        const colour = grid.style.backgroundColor;
+        event.target.style.backgroundColor = "orange";
+
+        // Reset the color after a 500ms delay.
+        setTimeout(function () {
+          event.target.style.backgroundColor = colour;
+        }, 500);
+      },
+      false
+    );
+  });
+}
+
+createContainer();
+handleHover();
